83ab22f79b72dc9d514b2e6dfbb0b0a6f499924b
Ahmed Castro <antares_43@hotmail.com>
2011-12-01 10:56:45 -0600

    imagenes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def getImagen producto
+    if producto==1
+      imagen="/medicinas/1.png"
+    end
+    if producto==2
+      imagen="/medicinas/2.png"
+    end
+    if producto==3
+      imagen="/medicinas/3.png"
+    end
+    if producto==4
+      imagen="/medicinas/4.png"
+    end
+    return imagen
+  end
 end
