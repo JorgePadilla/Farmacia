90ca13e60aee57a4ac935c6c016f3d3277ad9399
Ahmed Castro <antares_43@hotmail.com>
2011-12-01 14:03:59 -0600

    Tabla de medicinas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,6 +12,21 @@ module ApplicationHelper
     if producto==4
       imagen="/medicinas/4.png"
     end
+    if producto==5
+      imagen="/medicinas/5.png"
+    end
+    if producto==6
+      imagen="/medicinas/6.png"
+    end
+    if producto==7
+      imagen="/medicinas/7.png"
+    end
+    if producto==8
+      imagen="/medicinas/8.png"
+    end
+    if producto==9
+      imagen="/medicinas/9.png"
+    end
     return imagen
   end
 end
